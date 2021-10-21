bc21d488ea9caf57f038efea8b97e0d2e22769f6
halaveika <halaveika1988@gmail.com>
2021-10-21 16:50:23 +0300

    feat: add production server

---
+++ utils/prodServer.js
@@ -0,0 +1,16 @@
+const express = require( 'express' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+const app = express();
+app.get( /\.(js|css|map|ico)$/, express.static( path.resolve( __dirname, '../build' ) ) );
+app.use( '*', ( req, res ) => {
+let indexHTML = fs.readFileSync( path.resolve( __dirname, '../build/index.html' ), {
+    encoding: 'utf8',
+} );
+res.contentType( 'text/html' );
+res.status( 200 );
+return res.send( indexHTML );
+} );
+app.listen( 3000, () => {
+    console.log( 'Express server started at <http://localhost:3000>' );
+} );
